5595c2d039bd3f8e22eb4f13bfa2086e71433461
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-08-05 01:15:54 -0300

    Created GlobalStyles of the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import theme from './Styles/theme';
+import GlobalStyle from './styles/global';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme = { theme }>
+      <GlobalStyle />
+        <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+export default {
+  COLORS: {
+    BACKGROUND: "#312E38",
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+    GRAY: "#948F99"
+  }
+}
