6860ad7a7ca31dcf67669edbaed4807d47aea3ac
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-08-07 03:00:11 -0300

    Created Tag Component

---
+++ src/components/Tag/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.span`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.COLORS.WHITE_200};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+
+  padding: 0.5rem 1.6rem;
+  margin-right: 0.8rem;
+  border-radius: 0.8rem;
+`
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,7 @@ export default {
     BACKGROUND: '#312E38',
     BACKGROUND_900: '#262529',
     WHITE: '#F4EDE8',
+    WHITE_200: '#E5E5E5',
     PINK: '#FF859B',
     PINK_BACKGROUND: '#FF859B0D',
     GRAY_100: '#948F99'
