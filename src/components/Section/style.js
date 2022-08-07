b5fc3a044fad098c6d0fa2e7f53c18daf4ffffbd
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-08-07 02:48:28 -0300

    Created Section Component

---
+++ src/components/Section/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+  margin: 2.8rem 0;
+
+  > h2 {
+    font-weight: 400;
+    font-size: 3.2rem;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    padding-bottom: 1.6rem;
+    margin-bottom: 2.8rem;
+  }
+`
