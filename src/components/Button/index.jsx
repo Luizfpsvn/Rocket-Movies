a2e7c7095778736e826f358a5f3c9deb92c7c235
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-08-31 02:41:50 -0300

    Created user SignUp states and function to handle the data, so the user now can have messages, if the register is a sucess or a error. Fixed a bug, where the button component is now receiving rest properties well, now is fixed, and Register is fully operational

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import { Container } from "./style";
 
 export function Button({ title, loading, icon: Icon, ...rest }) {
   return (
-    <Container type="button" disabled={loading}>
+    <Container type="button" disabled={loading} {...rest}>
       {Icon && <Icon size={20} />}
       {loading ? "loading" : title}
     </Container>
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -2,12 +2,42 @@ import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
 
 import { Container, Form, Background } from "./styles";
 
+import { useState } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
+
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { TextLink } from "../../components/TextLink";
-import { Link } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      alert("You need to fill all the fields to complete your register!");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Sucess, now you are registered as user!");
+        navigate("/");
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Error, it was not possible to complete your register!");
+        }
+      });
+  }
+
   return (
     <Container>
       <Background />
@@ -18,11 +48,26 @@ export function SignUp() {
 
         <h2>Create your account</h2>
 
-        <Input type="text" icon={FiUser} placeholder="Name" />
-        <Input type="text" icon={FiMail} placeholder="E-mail" />
-        <Input type="password" icon={FiLock} placeholder="Password" />
+        <Input
+          type="text"
+          icon={FiUser}
+          placeholder="Name"
+          onChange={event => setName(event.target.value)}
+        />
+        <Input
+          type="text"
+          icon={FiMail}
+          placeholder="E-mail"
+          onChange={event => setEmail(event.target.value)}
+        />
+        <Input
+          type="password"
+          icon={FiLock}
+          placeholder="Password"
+          onChange={event => setPassword(event.target.value)}
+        />
 
-        <Button title="Register" />
+        <Button title="Register" onClick={handleSignUp} />
 
         <Link to="/">
           <FiArrowLeft /> Back
