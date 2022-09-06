b13ce243585956f6ea02821d878ee1a4a47b7b60
Luiz Fernando <luizfernando.cg@hotmail.com>
2022-09-06 11:34:18 -0300

    Now in the new movie Page, the markers is fully functional and they work when you add and remove them

--- src/pages/CreateMovie/index.jsx
+++ src/pages/CreateMovie/index.jsx
@@ -2,6 +2,8 @@ import { FiArrowLeft } from "react-icons/fi";
 
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+
 import { Container } from "./styles";
 
 import { Input } from "../../components/Input";
@@ -13,6 +15,18 @@ import { TextArea } from "../../components/TextArea";
 import { NoteItem } from "../../components/NoteItem";
 
 export function CreateMovie() {
+  const [markers, setMarkers] = useState([]);
+  const [newMarker, setNewMarker] = useState("");
+
+  function handleAddMarker() {
+    setMarkers(prevState => [...prevState, newMarker]);
+    setNewMarker("");
+  }
+
+  function handleRemoveMarker(removed) {
+    setMarkers(prevState => prevState.filter(marker => marker !== removed));
+  }
+
   return (
     <Container>
       <Header />
@@ -33,8 +47,21 @@ export function CreateMovie() {
 
         <Section title="Markers" />
         <div className="notes">
-          <NoteItem value="React" />
-          <NoteItem isNew placeholder="New Marker" />
+          {markers.map((marker, index) => (
+            <NoteItem
+              key={String(index)}
+              value={marker}
+              onClick={() => handleRemoveMarker(marker)}
+            />
+          ))}
+
+          <NoteItem
+            isNew
+            placeholder="New Marker"
+            value={newMarker}
+            onChange={event => setNewMarker(event.target.value)}
+            onClick={handleAddMarker}
+          />
         </div>
       </main>
 
